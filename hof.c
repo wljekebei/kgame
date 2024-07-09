856720ae218ffd72eaffccfd16715590620bebe8
student <student@SlovakstudyUbuntu.myguest.virtualbox.org>
2024-07-09 18:17:49 +0200

    everything fixed

--- hof.c
+++ hof.c
@@ -47,6 +47,10 @@ bool save(const struct player list[], const int size) {
         slist[i] = list[i].score;
     }
     FILE *f = fopen(HOF_FILE, "w");
+    if (f == NULL) {
+        printf("Error: Can not open HOF file");
+        return 0;
+    }
     for (int i = 0; i < size; i++) {
         int max = slist[0];
         int maxIndex = 0;
@@ -70,26 +74,26 @@ bool save(const struct player list[], const int size) {
 
 bool add_player(struct player list[], int* size, const struct player player) {
     if (*size < 10) {
-        list[*size+1].score = player.score;
+        list[*size].score = player.score;
+    }
+    if ((*size) < 10) {
+        list[(*size)] = player;
+        printf("before: %d\n", *size);
+        (*size)++;
+        printf("after: %d\n", *size);
+        if (save(list, *size)) {
+            printf("Your score was added to HOF list!\n");
+            return 1;
+        }
     }
     for (int i = 0; i < *size; i++) {
         if (player.score >= list[i].score) {
             list[(*size)-1] = player;
-            (*size)++;
             if (save(list, *size)) {
                 printf("Your best score was added to HOF list!\n");
                 return 1;
             }
-        } else if ((*size) < 10) {
-            list[(*size)] = player;
-            printf("before: %d\n", *size);
-            (*size)++;
-            printf("after: %d\n", *size);
-            if (save(list, *size)) {
-                printf("Your score was added to HOF list!\n");
-                return 1;
-            }
-        }
+        } 
     }
     return 0;
 }
